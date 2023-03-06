b0a7e3f5e8282a7d7396485ad1a2bd0ddf59b56f
Liam <64935867+8liam@users.noreply.github.com>
2023-03-07 00:16:04 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import './svg.css'
 import Home from "./pages/home";
 import About from "./pages/about";
 import Navigation from './components/Navigation'
+import Footer from './components/Footer'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 
 function App() {
   return (
-        
+        <>
         <Navigation />
-        
+        <Footer />
+        </>
   )
 }
 
